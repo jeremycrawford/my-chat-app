23ce8bdbb7f348986d50261fcabce40939add676
Jeremy Crawford <jcrawford.design@gmail.com>
2014-02-21 22:54:43 -0500

    Initial Commit

---
+++ app/scripts/main.js
@@ -0,0 +1,43 @@
+console.log('This is our chat app!');
+
+$(document).ready(function(){
+
+  profile = new ProfileCollection();
+
+  profile.fetch({
+    success: function(){
+      profile.each(function(message){
+        new MessageView({model: message});
+        $('.full-list').scrollTop($('.full-list')[0].scrollHeight);
+      })
+    },
+
+    error: function(){
+      console.log('error')
+    }
+  });
+
+
+ newUser = $('.add-album-button').click(function(){
+
+ $('.js-username').val()
+ });
+
+
+
+
+  $('.js-input-submit').click(function(){
+
+    var message = new Profile()
+
+      if($('.js-message-input').val()) {message.set({messageText: $('.js-message-input').val()})};
+      if(newUser) {message.set({username: $('.js-username').val()})};
+
+    var freshModel = profile.add(message);
+
+    new MessageView({model: freshModel});
+
+    freshModel.save()
+  });
+
+});
---
+++ app/scripts/message-list.js
@@ -0,0 +1,21 @@
+var MessageView = Backbone.View.extend({
+  className: 'new-message',
+
+  createTemplate: _.template($('#message-list-template').text()),
+
+
+  initialize: function(){
+    $('.js-full-list').prepend( this.el );
+    
+    this.render();
+
+  },
+
+  render: function(){
+    var renderedTemplate = this.createTemplate( this.model.attributes )
+
+    this.$el.html( renderedTemplate );
+
+  }
+
+});
